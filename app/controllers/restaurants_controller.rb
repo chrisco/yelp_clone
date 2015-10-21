f151be17c3b965c7270db9e0a8b0ee97370e840b
Bo Mattsson <bo.mattsson@cint.com>
2015-10-21 10:49:58 +0200

    added restaurant name length validation

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,7 +10,11 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.create(restaurant_params)
-    redirect_to '/restaurants'
+    if @restaurant.save
+      redirect_to restaurant_path
+    else
+      render 'new'
+    end
   end
 
   def restaurant_params
